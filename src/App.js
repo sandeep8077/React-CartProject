65ff2a4181a5a2a8f1cea7b5adb954d290aff9c4
Sandeep <sk557618269@gmail.com>
2023-03-23 13:14:58 +0530

    Add firebase and  fetch data on to browser

--- src/App.js
+++ src/App.js
@@ -1,135 +1,120 @@
-
 import './index';
 import React from 'react';
 import Cart from './Cart'
 import NavBar from './NavBar';
 import Footer from './Footer';
+// import * as firebase from 'firebase';
+import firebase from 'firebase/compat/app';
 
 class App extends React.Component {
-  constructor(){
-    super()
-    this.state={
-        products:[
-            {
-                title:'I phone 12pro',
-                price:127000,
-                qnt:2,
-                img:"https://m.media-amazon.com/images/I/610pghkO81L._SX679_.jpg" ,
-                id:1
-            },
-            {
-                title:'laptop',
-                price:80000,
-                qnt:2,
-                img:'https://m.media-amazon.com/images/I/41YamNJ29mL._SX300_SY300_.jpg',
-                id:2
-            },
-            {
-                title:'Watch',
-                price:1799,
-                qnt:2,
-                img:'https://m.media-amazon.com/images/I/41WCgGbvwhL._SY300_SX300_.jpg',
-                id:3
-            },{
-                title:'Bat',
-                price:500,
-                qnt:2,
-                img:'https://m.media-amazon.com/images/I/41nvB58-9aL.jpg',
-                id:4
-            }
-            
-  
-        ]
+    constructor() {
+        super()
+        this.state = {
+            products: [],
+            loading:true,
+        }
     }
-  }
-  
-  // handle functionality to increase quantity
-  handleIncreaseQty = (product)=>{
-    console.log("work Increase functionality",product);
-   const {products} = this.state;
-   const index = products.indexOf(product)
-   products[index].qnt+=1;
-   
-   this.setState({
-      products:products
-    })
-  }
-  
-  // decreasing qnt of products
-  handleDecreaseQnt =(product)=>{
-    console.log("decreasing quantity is work",product);
-   
-   if(product.qnt>0){
-    const {products}= this.state;
-    const index = products.indexOf(product);
-    products[index].qnt-=1;
-    this.setState({
-        products:products
-    })
-   }
-  }
-  
-  // handle delete item from the cart
-  handleDeleteItem = (id)=>{
-    const {products} = this.state;
-    const items = products.filter((item)=>{ return(item.id !== id)}
-        
-    )
-    this.setState({
-        products:items
-    })
-  }
 
-  // count the cart items 
-  getCount(){
-    let count = 0;
-    const {products} = this.state;
-    products.forEach((product)=>{
-      count+= product.qnt;
-    })
-    return count
-  }
+    componentDidMount() {
+        firebase
+            .firestore()
+            .collection('products')
+            .onSnapshot((snapshot) => {
+              // console.log(snapshot);
+            const products= snapshot.docs.map((doc)=>{
+              const data = doc.data();
+              data['id']=doc.id
+               return data;
+             })
 
-  // Get total price of the products
-  totelPrice(){
-    let totel = 0;
-    const {products} = this.state;
-    products.forEach((product)=>{
-     totel+= product.qnt*product.price;
-    })
-    return totel;
-  }
+             this.setState({
+              products:products,
+              loading:false
+             })
+            })
+            
+
+    }
+
+    // handle functionality to increase quantity
+    handleIncreaseQty = (product) => {
+        // console.log("work Increase functionality", product);
+        const { products } = this.state;
+        const index = products.indexOf(product)
+        products[index].Qty += 1;
+
+        this.setState({
+            products: products
+        })
+    }
+
+    // decreasing qnt of products
+    handleDecreaseQnt = (product) => {
+        // console.log("decreasing quantity is work", product);
+
+        if (product.Qty > 0) {
+            const { products } = this.state;
+            const index = products.indexOf(product);
+            products[index].Qty -= 1;
+            this.setState({
+                products: products
+            })
+        }
+    }
+
+    // handle delete item from the cart
+    handleDeleteItem = (id) => {
+        const { products } = this.state;
+        const items = products.filter((item) => { return (item.id !== id) }
+
+        )
+        this.setState({
+            products: items
+        })
+    }
+
+    // count the cart items 
+    getCount() {
+        let count = 0;
+        const { products } = this.state;
+        products.forEach((product) => {
+            count += product.Qty;
+        })
+        return count
+    }
+
+    // Get total price of the products
+    totelPrice() {
+        let totel = 0;
+        const { products } = this.state;
+        products.forEach((product) => {
+            totel += product.Qty * product.price;
+        })
+        return totel;
+    }
 
 
 
-  render(){
-    const {products}=this.state;
-    return (
-      <div className="App">
-     
-        <NavBar count={this.getCount()}/>
-        <Cart
-          products ={products}
-          qntOnChangeIncrease={this.handleIncreaseQty}
-          qntOnChangeDecrease={this.handleDecreaseQnt}
-          deleteItems={this.handleDeleteItem}
-        />
-        <Footer totelPrice={this.totelPrice()} />
-     
-      </div>
-    );
-    
-  }
-  
- 
+    render() {
+        const { products,loading } = this.state;
+        return (
+           <div className = "App" >
+
+            <NavBar count = { this.getCount() }/>
+            <Cart products = { products }
+            qntOnChangeIncrease = { this.handleIncreaseQty }
+            qntOnChangeDecrease = { this.handleDecreaseQnt }
+            deleteItems = { this.handleDeleteItem }/>
+            {loading && <h2>Loading products...</h2>}
+            <Footer totelPrice = { this.totelPrice() }/>
+          
+
+            </div>
+        );
+
+    }
+
+
 }
 
 export default App;
-
-
-
-
-
-
-
-
--- src/index.js
+++ src/index.js
@@ -2,7 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB_Gq_IaEtHkkayT01XgmD_-BZrVVMTcQk",
+  authDomain: "cart-87f72.firebaseapp.com",
+  projectId: "cart-87f72",
+  storageBucket: "cart-87f72.appspot.com",
+  messagingSenderId: "80060500034",
+  appId: "1:80060500034:web:742ff3468c3b6bc16405db"
+};
+// Initialize Firebase
+const firebaseApp= firebase.initializeApp(firebaseConfig);
+// Use these for db & auth
+const db = firebaseApp.firestore();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,4 +28,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+export {db}
 
